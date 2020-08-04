0b914da590be0ed1e58665d9f17c2f5e7a307d11
gambit <gam_bit91@naver.com>
2020-08-04 15:07:34 +0900

    Add naver_movie.py file

---
+++ test-project/naver_movie.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+url = "https://movie.naver.com/movie/running/current.nhn"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+movie_selection = soup.select('div[id=content] > div.article > div.obj_section > div.lst_wrap > ul.lst_detail_t1 > li')
+
+movie_data = []
+
+for movie in movie_selection:
+    
+    a_tag = movie.select_one('dl > dt > a')
+    
+    movie_title = a_tag.text
+    movie_code = a_tag['href'].split('=')[1]
+
+
+    movie_dict = {
+        'title': movie_title,
+        'code': movie_code
+    }
+    
+    movie_data.append(movie_dict)
+ 
+
+print(movie_data)
+
+
+
+
+
