9fccbaae353d5ba02d5d68c03c79fb5ef32f8408
gambit <gam_bit91@naver.com>
2020-08-06 16:25:55 +0900

    Web scraping 네이버 영화 리뷰 전체 코드

--- test-project/naver_movie.py
+++ test-project/naver_movie.py
@@ -28,13 +28,68 @@ for movie in movie_selection:
     movie_data.append(movie_dict)
  
 
-print(movie_data)
+# print(movie_data)
 
 
-with open('./data/naver_movie.csv', 'w') as csvfile:
-    filednames = ['title', 'code']
-    writer = csv.DictWriter(csvfile, fieldnames=filednames, lineterminator = '\n')
+# with open('./data/naver_movie.csv', 'w') as csvfile:
+#     filednames = ['title', 'code']
+#     writer = csv.DictWriter(csvfile, fieldnames=filednames, lineterminator = '\n')
+#     writer.writeheader()
+#     writer.writerows(movie_data)
+
+
+
+movie_reviews = []
+for movie in movie_data:
+   
+    params = (
+        ('code', movie['code']),
+        ('type', 'after'),
+        ('isActualPointWriteExecute', 'false'),
+        ('isMileageSubscriptionAlready', 'false'),
+        ('isMileageSubscriptionReject', 'false'),
+    )
+
+    response = requests.get('https://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn', params=params)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    reviews = soup.select('body > div > div > div.score_result > ul > li')
+    count = 0
+
+    
+    for review in reviews:
+        review_dict = {}
+        star_score = review.select_one('div.star_score > em').text
+        if not review.select(f'div.score_reple > p > span#_filtered_ment_{count} > span#_unfold_ment{count} > a'): 
+            reple = review.select_one(f'div.score_reple > p > span#_filtered_ment_{count}').text.strip()
+        else:
+            reple = review.select_one(f'div.score_reple > p > span#_filtered_ment_{count} > span#_unfold_ment{count} > a')['data-src']
+        
+        count += 1
+
+        review_dict['title'] = movie['title']
+        review_dict['code'] = movie['code']
+        review_dict['star_score'] = star_score
+        review_dict['reple'] = reple
+        movie_reviews.append(review_dict)
+
+
+with open('./data/naver_movie_reviews.csv', 'w') as csvfile:
+    filednames = ['title', 'code', 'star_score', 'reple']
+    writer = csv.DictWriter(csvfile, fieldnames=filednames, lineterminator='\n')
     writer.writeheader()
-    writer.writerows(movie_data)
+    writer.writerows(movie_reviews)
+
+
+
+    
+
+        
+
+        
+
+
+
 
 
